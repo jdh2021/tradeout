2ec5d174710bbce1c67bf05b4aab2b0be9dc4014
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-11-26 16:48:20 -0600

    ContractDetails basic code to retrieve details from server. No framework yet. Working back to front. Waiting on PR approval

--- src/components/ContractDetails/ContractDetails.jsx
+++ src/components/ContractDetails/ContractDetails.jsx
@@ -1,9 +1,25 @@
 import React, { useState } from 'react';
 
 function ContractDetails() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const contractDetails = useSelector(store => store.selectedContract)
+  const {contractId} = useParams();
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_CONTRACT_DETAILS', payload: contractId })
+  })
+
+//   const formatDate = (dateString) => {
+//     const date = new Date(dateString);
+//     return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+// }
+
   return (
     <div>
         <h1>Contract Details</h1>
+        <pre>{JSON.stringify(selectedContract)}</pre>
+
     </div>
   );
 }
