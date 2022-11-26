9379c57878b250a3f2715ad287cc4510d495e25d
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-11-26 14:37:22 -0600

    added 'Edit Contract Details' and 'Recipient Contact Infomation' buttons below review table

--- src/components/CreateContract/CreateContractReview.jsx
+++ src/components/CreateContract/CreateContractReview.jsx
@@ -10,10 +10,10 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 const CreateContractReview = () => {
 
-  const dispatch = useDispatch();
   const history = useHistory();
   const newContractDetails = useSelector(store => store.contract.newContractDetails);
 
@@ -35,50 +35,69 @@ const CreateContractReview = () => {
             <Table>
               <TableBody>
                 <TableRow>
-                  <TableCell align="left"><Typography>Contract Title:</Typography></TableCell>
+                  <TableCell sx={{width: 150}} align="left"><Typography>Contract Title:</Typography></TableCell>
                   <TableCell align="left">{newContractDetails.contractTitle}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell align="left"><Typography>Item Name:</Typography></TableCell>
+                  <TableCell sx={{width: 150}} align="left"><Typography>Item Name:</Typography></TableCell>
                   <TableCell align="left">{newContractDetails.itemName}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell align="left"><Typography>Item Description:</Typography></TableCell>
+                  <TableCell sx={{width: 150}} align="left"><Typography>Item Description:</Typography></TableCell>
                   <TableCell align="left">{newContractDetails.itemDescription}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell align="left"><Typography>Item Price:</Typography></TableCell>
+                  <TableCell sx={{width: 150}} align="left"><Typography>Item Price:</Typography></TableCell>
                   <TableCell align="left">${newContractDetails.itemPrice}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell align="left"><Typography>Pickup Location:</Typography></TableCell>
+                  <TableCell sx={{width: 150}} align="left"><Typography>Pickup Location:</Typography></TableCell>
                   <TableCell align="left">{newContractDetails.pickupLocation}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell align="left"><Typography>Pickup Date:</Typography></TableCell>
+                  <TableCell sx={{width: 150}} align="left"><Typography>Pickup Date:</Typography></TableCell>
                   <TableCell align="left">{formattedPickupDate}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell align="left"><Typography>Contract Deadline:</Typography></TableCell>
+                  <TableCell sx={{width: 150}} align="left"><Typography>Contract Deadline:</Typography></TableCell>
                   <TableCell align="left">{formattedContractDeadline}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell align="left"><Typography>Notes:</Typography></TableCell>
+                  <TableCell sx={{width: 150}} align="left"><Typography>Notes:</Typography></TableCell>
                   <TableCell align="left">{newContractDetails.contractNotes}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell align="left"><Typography>Your Signature:</Typography></TableCell>
+                  <TableCell sx={{width: 150}} align="left"><Typography>Your Signature:</Typography></TableCell>
                   <TableCell align="left">{newContractDetails.firstPartySignature}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell align="left"><Typography>Images:</Typography></TableCell>
+                  <TableCell sx={{width: 150}} align="left"><Typography>Images:</Typography></TableCell>
+                  {/* once image upload is enabled, the img src will be the uploaded image file */}
                   <TableCell align="left"><img src="https://images.unsplash.com/photo-1531104985437-603d6490e6d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1678&q=80" width="200"/></TableCell>
                 </TableRow>
               </TableBody>
             </Table>
           </TableContainer>
-          
         </Container>
+        <br />
+        <br />
+        <Box sx={{display: 'flex', justifyContent: 'center'}}>
+            <Button 
+              variant="contained"
+              onClick={(event) => history.push('/create-contract-details')}
+              sx={{marginRight: 1, width: 200}}
+            >
+              Edit Contract Details
+            </Button>
+            {/* Preview button here */}
+            <Button 
+              variant="contained"
+              onClick={(event) => history.push('/send-to-recipient')}
+              sx={{marginLeft: 1, width: 200}}
+            >
+              Recipient Contact Information
+            </Button>
+          </Box>
     </div>
   );
 
