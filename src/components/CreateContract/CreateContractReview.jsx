197d2db4cd6082333e556ab7326e6df36c115c44
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-11-26 14:22:45 -0600

    created MUI table to display user-entered new contract details

--- src/components/CreateContract/CreateContractReview.jsx
+++ src/components/CreateContract/CreateContractReview.jsx
@@ -1,10 +1,84 @@
 import { useState } from 'react';
+import {useHistory} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const CreateContractReview = () => {
 
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const newContractDetails = useSelector(store => store.contract.newContractDetails);
+
+  // date formatting for pickup date
+  const pickupDate = new Date(newContractDetails.pickupDate);
+  const formattedPickupDate = pickupDate.toLocaleDateString('en-US', { year: '2-digit', month: '2-digit', day: '2-digit' });
+
+  // date formatting for contract deadline
+  const contractDeadline = new Date(newContractDetails.contractDeadline);
+  const formattedContractDeadline = contractDeadline.toLocaleDateString('en-US', { year: '2-digit', month: '2-digit', day: '2-digit' });
+
   return (
     <div>
-        <h1>CreateContractReview</h1>
+        <Typography variant="h3" sx={{textAlign: "center"}}>Review New Contract Details</Typography>
+        <br />
+        <br />
+        <Container sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          <TableContainer elevation={10} component={Paper} sx={{width: 700}}>
+            <Table>
+              <TableBody>
+                <TableRow>
+                  <TableCell align="left"><Typography>Contract Title:</Typography></TableCell>
+                  <TableCell align="left">{newContractDetails.contractTitle}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left"><Typography>Item Name:</Typography></TableCell>
+                  <TableCell align="left">{newContractDetails.itemName}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left"><Typography>Item Description:</Typography></TableCell>
+                  <TableCell align="left">{newContractDetails.itemDescription}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left"><Typography>Item Price:</Typography></TableCell>
+                  <TableCell align="left">${newContractDetails.itemPrice}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left"><Typography>Pickup Location:</Typography></TableCell>
+                  <TableCell align="left">{newContractDetails.pickupLocation}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left"><Typography>Pickup Date:</Typography></TableCell>
+                  <TableCell align="left">{formattedPickupDate}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left"><Typography>Contract Deadline:</Typography></TableCell>
+                  <TableCell align="left">{formattedContractDeadline}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left"><Typography>Notes:</Typography></TableCell>
+                  <TableCell align="left">{newContractDetails.contractNotes}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left"><Typography>Your Signature:</Typography></TableCell>
+                  <TableCell align="left">{newContractDetails.firstPartySignature}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell align="left"><Typography>Images:</Typography></TableCell>
+                  <TableCell align="left"><img src="https://images.unsplash.com/photo-1531104985437-603d6490e6d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1678&q=80" width="200"/></TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          
+        </Container>
     </div>
   );
 
