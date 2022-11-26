8076de5565f9db447b4781011c72dc6f6a8ab8df
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-11-26 09:31:55 -0600

    using newContractDetails reducer, onChange of radio button group, the user is set to either buyer or seller. onClick of the 'Next' button, the secondPartyType is set to the opposite of the user selection and the user is pushed to CreateContractDetails

--- src/components/CreateContract/PartyType.jsx
+++ src/components/CreateContract/PartyType.jsx
@@ -12,37 +12,48 @@ const PartyType = () => {
 
   const dispatch = useDispatch();
   const history = useHistory();
-  // const newContractDetails = useSelector(store => store.contract.newContractDetails);
+  const newContractDetails = useSelector(store => store.contract.newContractDetails);
+  const [secondParty, setSecondParty] = useState('');
 
-  // const handleChangeFor = (key) => (event) => {
-  //   console.log('in handleChangeFor');
-  //   dispatch({type: 'SET_NEW_CONTRACT_DETAILS', payload: {...newContractDetails, [key]: event.target.value}});
-  //   if (event.target.value === 'buyer') {
-  //     dispatch({type: 'SET_NEW_CONTRACT_DETAILS', payload: {...newContractDetails, [secondPartyType]: 'seller'}});
-  //   } else if (event.target.value === 'seller') {
-  //     dispatch({type: 'SET_NEW_CONTRACT_DETAILS', payload: {...newContractDetails, [secondPartyType]: 'buyer'}});
-  //   }
-  // }
+  const handleChangeFor = (key) => (event) => {
+    console.log('in handleChangeFor', event.target.value);
+    if (event.target.value === 'buyer') {
+      setSecondParty('seller');
+    } else if (event.target.value === 'seller') {
+      setSecondParty('buyer');
+    }
+    dispatch({type: 'SET_NEW_CONTRACT_DETAILS', payload: {...newContractDetails, [key]: event.target.value}});
+  }
+
+  const handleChangeForSecondParty = (contractDetail, partyType) => {
+    console.log('in handleChangeForSecondParty', contractDetail, partyType);
+    if (partyType === 'buyer') {
+      dispatch({type: 'SET_NEW_CONTRACT_DETAILS', payload: {...newContractDetails, [contractDetail]: 'buyer'}});
+    } else if (partyType === 'seller') {
+      dispatch({type: 'SET_NEW_CONTRACT_DETAILS', payload: {...newContractDetails, [contractDetail]: 'seller'}});
+    }
+    history.push('/create-contract-details');
+  }
 
   // for testing the value being set onChange
-  const [firstPartyType, setFirstPartyType] = useState('');
-  const handleChange = (partyType) => {
-    console.log('in handleChange', partyType);
-    setFirstPartyType(partyType);
-  }
-  const toForm = () => {
-    console.log('firstPartyType', firstPartyType);
-  }
+  // const [firstPartyType, setFirstPartyType] = useState('');
+  // const handleChange = (partyType) => {
+  //   console.log('in handleChange', partyType);
+  //   setFirstPartyType(partyType);
+  // }
+  // const toForm = () => {
+  //   console.log('firstPartyType', firstPartyType);
+  // }
 
   return (
     <div>
         <h1>Are you a buyer or seller?</h1>
         <FormControl>
           <RadioGroup
-            // value={newContractDetails.firstPartyType}
-            // onChange={handleChangeFor('firstPartyType')}
-            value={firstPartyType}
-            onChange={(event) => handleChange(event.target.value)} 
+            value={newContractDetails.firstPartyType}
+            onChange={handleChangeFor('firstPartyType')}
+            // value={firstPartyType}
+            // onChange={(event) => handleChange(event.target.value)} 
           >
             <FormControlLabel value="buyer" control={<Radio />} label="Buyer" />
             <FormControlLabel value="seller" control={<Radio />} label="Seller" />
@@ -50,7 +61,7 @@ const PartyType = () => {
         </FormControl>
         <br />
         <br />
-        <Button variant="contained" onClick={() => history.push('/create-contract-details')}>Next</Button>
+        <Button variant="contained" onClick={() => handleChangeForSecondParty('secondPartyType', secondParty)}>Next</Button>
     </div>
   );
 
