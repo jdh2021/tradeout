cf552012419553f06aae9d86ce74cd67bcac3bcf
Oscar Carbajal <Oscar.carbajal.work@gmail.com>
2022-11-26 23:28:39 -0600

    tested without redux and formatting looks good. Once PRs are approved we can insert data from the server

--- src/components/ContractDetails/ContractDetails.jsx
+++ src/components/ContractDetails/ContractDetails.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
@@ -17,10 +17,12 @@ function ContractDetails() {
   const history = useHistory();
   const contractDetails = useSelector(store => store.selectedContract)
   const { contractId } = useParams();
+  // need user reducer in the case we need to pull out the user's email? 
+  // const user = useSelector((store) => store.user)
 
   useEffect(() => {
     dispatch({ type: 'FETCH_CONTRACT_DETAILS', payload: contractId })
-  })
+  }, [contractId])
 
   //format dates on contract 
   const formatDate = (dateString) => {
@@ -30,52 +32,81 @@ function ContractDetails() {
 
   return (
     <div>
+      {/* page heading. May be useful to pass contract status in heading with reducer ex:" PendingContract Details" */}
+      <Typography variant="h3" sx={{ textAlign: "center" }}>
+        {contractDetails.contractStatus}Contract Details</Typography>
+      <br />
+      <br />
+
       <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <TableContainer elevation={10} component={Paper} sx={{ width: 700 }}>
           <Table>
             <TableBody>
+
               <TableRow>
-                <TableCell sx={{ width: 150 }} align="left"><Typography>Contract Title:</Typography></TableCell>
-                <TableCell align="left">{selectedContract.contractTitle}</TableCell>
+                <TableCell sx={{ width: 150 }} align="left">
+                  <Typography>Contract Title:</Typography></TableCell>
+                <TableCell align="left">{contractDetails.contractTitle}</TableCell>
               </TableRow>
+
               <TableRow>
-                <TableCell sx={{ width: 150 }} align="left"><Typography>Item Name:</Typography></TableCell>
-                <TableCell align="left">{selectedContract.itemName}</TableCell>
+                <TableCell sx={{ width: 150 }} align="left">
+                  <Typography>Item Name:</Typography></TableCell>
+                <TableCell align="left">{contractDetails.itemName}</TableCell>
               </TableRow>
+
               <TableRow>
-                <TableCell sx={{ width: 150 }} align="left"><Typography>Item Description:</Typography></TableCell>
-                <TableCell align="left">{selectedContract.itemDescription}</TableCell>
+                <TableCell sx={{ width: 150 }} align="left">
+                  <Typography>Item Description:</Typography></TableCell>
+                <TableCell align="left">{contractDetails.itemDescription}</TableCell>
               </TableRow>
+
               <TableRow>
-                <TableCell sx={{ width: 150 }} align="left"><Typography>Item Price:</Typography></TableCell>
-                <TableCell align="left">${selectedContract.itemPrice}</TableCell>
+                <TableCell sx={{ width: 150 }} align="left">
+                  <Typography>Item Price:</Typography></TableCell>
+                <TableCell align="left">${contractDetails.itemPrice}</TableCell>
               </TableRow>
+
               <TableRow>
-                <TableCell sx={{ width: 150 }} align="left"><Typography>Pickup Location:</Typography></TableCell>
-                <TableCell align="left">{selectedContract.pickupLocation}</TableCell>
+                <TableCell sx={{ width: 150 }} align="left">
+                  <Typography>Pickup Location:</Typography></TableCell>
+                <TableCell align="left">{contractDetails.pickupLocation}</TableCell>
               </TableRow>
+
               <TableRow>
-                <TableCell sx={{ width: 150 }} align="left"><Typography>Pickup Date:</Typography></TableCell>
-                <TableCell align="left">{formatDate(selectedContract.pickupDate)}</TableCell>
+                <TableCell sx={{ width: 150 }} align="left">
+                  <Typography>Pickup Date:</Typography></TableCell>
+                <TableCell align="left">{formatDate(contractDetails.pickupDate)}</TableCell>
               </TableRow>
+
               <TableRow>
-                <TableCell sx={{ width: 150 }} align="left"><Typography>Contract Deadline:</Typography></TableCell>
-                <TableCell align="left">{selectedContract.deadline}</TableCell>
+                <TableCell sx={{ width: 150 }} align="left">
+                  <Typography>Contract Deadline:</Typography></TableCell>
+                <TableCell align="left">{contractDetails.deadline}</TableCell>
               </TableRow>
+
               <TableRow>
-                <TableCell sx={{ width: 150 }} align="left"><Typography>Notes:</Typography></TableCell>
-                <TableCell align="left">{selectedContract.contractNotes}</TableCell>
+                <TableCell sx={{ width: 150 }} align="left">
+                  <Typography>Notes:</Typography></TableCell>
+                <TableCell align="left">{contractDetails.contractNotes}</TableCell>
               </TableRow>
+
               <TableRow>
-                <TableCell sx={{ width: 150 }} align="left"><Typography>Your Signature:</Typography></TableCell>
-                <TableCell align="left">{selectedContract.firstPartySignature}</TableCell>
+                <TableCell sx={{ width: 150 }} align="left">
+                  <Typography>Your Signature:</Typography></TableCell>
+                <TableCell align="left">{contractDetails.firstPartySignature}</TableCell>
               </TableRow>
+
               <TableRow>
-                <TableCell sx={{ width: 150 }} align="left"><Typography>Second Party's Signature:</Typography></TableCell>
-                <TableCell align="left">{selectedContract.secondPartySignature}</TableCell>
+                <TableCell sx={{ width: 150 }} align="left">
+                  <Typography>Second Party's Signature:</Typography></TableCell>
+                <TableCell align="left">{contractDetails.secondPartySignature}</TableCell>
               </TableRow>
+
               <TableRow>
-                <TableCell sx={{ width: 150 }} align="left"><Typography>Images:</Typography></TableCell>
+                <TableCell sx={{ width: 150 }} align="left">
+                  <Typography>Images:</Typography></TableCell>
+
                 {/* once image upload is enabled, the img src will be the uploaded image file */}
                 <TableCell align="left">
                   <img src="https://i.ebayimg.com/images/g/ZUkAAOSw0x1jd7sn/s-l500.jpg"
