5e7aba50994e22f43842ab2e3f3f8dfa3b1125e1
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-11-28 09:24:12 -0600

    updated view title and email textfield

--- src/components/CreateContract/SendToRecipient.jsx
+++ src/components/CreateContract/SendToRecipient.jsx
@@ -1,10 +1,37 @@
 import { useState } from 'react';
+import {useHistory} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 const SendToRecipient = () => {
 
+  const history = useHistory();
+  const newContractDetails = useSelector(store => store.contract.newContractDetails);
+
+  // date formatting for pickup date
+  const pickupDate = new Date(newContractDetails.pickupDate);
+  const formattedPickupDate = pickupDate.toLocaleDateString('en-US', { year: '2-digit', month: '2-digit', day: '2-digit' });
+
+  // date formatting for contract deadline
+  const contractDeadline = new Date(newContractDetails.contractDeadline);
+  const formattedContractDeadline = contractDeadline.toLocaleDateString('en-US', { year: '2-digit', month: '2-digit', day: '2-digit' });
+
   return (
     <div>
-        <h1>SendToRecipient</h1>
+        <Box sx={{display: 'flex', justifyContent: 'center'}}>
+          <Typography variant="h3" sx={{textAlign: "center"}}>Send to Recipient:</Typography>
+          <TextField
+            sx={{width: 300, marginLeft: 2}}
+            helperText="Enter Recipient's Email"
+            label="example@gmail.com"
+          />
+        </Box>
+
     </div>
   );
 
