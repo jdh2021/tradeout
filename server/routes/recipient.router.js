7d972319b2bd9ae7e1348ef4ec981c69f9bebcf0
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-11-28 16:31:04 -0600

    created server route to GET contract by contract key within recipient.router.js

--- server/routes/recipient.router.js
+++ server/routes/recipient.router.js
@@ -2,5 +2,18 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+// GET contract by contract key. user not required to be logged in
+router.get('/:id', (req, res) => {
+    console.log('in /api/recipient GET contract by contract key');
+    console.log('contract key is:', req.params.id);
+    const queryText =   `SELECT * FROM "contract"
+                        WHERE "contract"."contract_key" = $1;`;
+    pool.query(queryText, [req.params.id]).then(result => {
+        res.send(result.rows[0]);
+    }).catch(error => {
+        console.log('Error in /api/recipient GET contract by contract key:', error);
+        res.sendStatus(500);
+    });
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use('/api/recipient', recipientContractRouter);
 app.use(express.static('build'));
 
 // App Set //
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5007;
 
 /** Listen * */
 app.listen(PORT, () => {
