d7e8c540e846448f25e3431c96022b0c0a1e458b
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-11-28 08:59:23 -0600

    updated server.js to include recipientContractRouter at '/api/recipient'

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const contractRouter = require('./routes/contract.router');
+const recipientContractRouter = require('./routes/contract.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/contract', contractRouter);
+app.use('/api/recipient', recipientContractRouter);
 
 // Serve static files
 app.use(express.static('build'));
