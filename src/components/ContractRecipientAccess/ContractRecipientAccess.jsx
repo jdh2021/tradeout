4b6c0c36cc27d25645cfdeb23ad20e78b6c4cbec
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-11-29 15:53:31 -0600

    added MUI components required TextField to enter contract key and button to 'View Contract'. wrapped components in form.

--- src/components/ContractRecipientAccess/ContractRecipientAccess.jsx
+++ src/components/ContractRecipientAccess/ContractRecipientAccess.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
 const ContractRecipientAccess = () => {
@@ -14,6 +18,26 @@ const ContractRecipientAccess = () => {
   return (
     <div>
       <Typography variant="h3" sx={{ textAlign: "center" }}>Access Contract</Typography>
+      <br />
+      <br />
+      <Grid container spacing={2} direction="column" alignItems="center" justifyContent="center">
+        <Grid item>
+          <form>
+            <TextField
+              required
+              fullWidth
+              label="Contract Key"
+              size="small"
+              helperText="Enter the contract key provided in the TradeOut email."
+              value={searchContractKey}
+              onChange={(event) => setSearchContractKey(event.target.value)}
+            />
+            <Box textAlign="center" sx={{ mt: 4 }}>
+              <Button type="submit" variant="contained">View Contract</Button>
+            </Box>
+          </form>
+        </Grid>
+      </Grid>
     </div>
   );
 }
