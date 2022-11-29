380ebd0fb2353c600ea2b5c3648880b3ed10cd20
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-11-28 18:24:58 -0600

    updated code comment for router to GET contract by contract key

--- server/routes/recipient.router.js
+++ server/routes/recipient.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-// GET contract by contract key. user not required to be logged in
+// GET contract by contract key sent to recipient email. user not required to be logged in.
 router.get('/:id', (req, res) => {
     console.log('in /api/recipient GET contract by contract key');
     console.log('contract key is:', req.params.id);
