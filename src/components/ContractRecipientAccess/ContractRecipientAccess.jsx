9ee3431ad74dfad4e481c14ed33ca7efb8bf8603
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-11-29 15:24:41 -0600

    initial setup for ContractRecipientAccess component. added local state for changeable value of contract key being searched.

--- src/components/ContractRecipientAccess/ContractRecipientAccess.jsx
+++ src/components/ContractRecipientAccess/ContractRecipientAccess.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
 
-// Basic functional component structure for React with default state
-// value setup. When making a new component be sure to replace the
-// component name TemplateFunction with the name for the new component.
-function ContractRecipientAccess(props) {
-  // Using hooks we're creating local state for a "heading" variable with
-  // a default value of 'Functional Component'
-  const store = useSelector((store) => store);
-  const [heading, setHeading] = useState('Contract Recipient Access');
+const ContractRecipientAccess = () => {
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  // local state for changeable values of contract key being searched
+  const [searchContractKey, setSearchContractKey] = useState('');
 
   return (
     <div>
-      <h2>{heading}</h2>
+      <Typography variant="h3" sx={{ textAlign: "center" }}>Access Contract</Typography>
     </div>
   );
 }
