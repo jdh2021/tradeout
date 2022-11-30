15689f46e3a45fe029a24a7996bd66f85f9240dd
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-11-30 08:10:15 -0600

    imported useParams in RecipientView component to pull client-side URL param from ULR generated in ContractAccessRecipient component. added useEffect in RecipientView to dispatch 'FETCH_RECIPIENT_CONTRACT'. tested routing between components to confirm correct contract selected and set in selectedContract reducer when matching key searched.

--- src/components/RecipientView/RecipientView.jsx
+++ src/components/RecipientView/RecipientView.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 function RecipientView() {
+  const dispatch = useDispatch();
+  // client-side URL param, corresponds to route exact path in App.jsx
+  const { searchContractKey } = useParams();
+
+  // dispatches FETCH_RECIPIENT_CONTRACT whenever searchContractKey changes
+  useEffect(() => {
+    dispatch({ type: 'FETCH_RECIPIENT_CONTRACT', payload: searchContractKey });
+  }, [searchContractKey]);
+
   return (
     <div>
-        <h1>RecipientView</h1>
+      <h1>RecipientView</h1>
     </div>
   );
 }
