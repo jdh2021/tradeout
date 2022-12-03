26094ea1bd5dc12e9179317c11f5df9a8d034513
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-12-03 12:06:20 -0600

    created acceptContract onClick function for Accept button that alerts recipient they'll be routed to registration, dispatches an action of 'SET_CONTRACT_KEY' to store the contract key in the reducer, and navigates user to '/registration.'

--- src/components/RecipientView/RecipientView.jsx
+++ src/components/RecipientView/RecipientView.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import ContractPreview from '../ContractPreview/ContractPreview';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
 function RecipientView() {
+  const history = useHistory();
   const dispatch = useDispatch();
   // client-side URL param, corresponds to route exact path in App.jsx
   const { searchContractKey } = useParams();
@@ -16,29 +18,36 @@ function RecipientView() {
     dispatch({ type: 'FETCH_RECIPIENT_CONTRACT', payload: searchContractKey });
   }, [searchContractKey]);
 
+  // navigates contract recipient to registration and stores contract key in reducer when "accept" is clicked
+  const acceptContract = () => {
+    console.log('in acceptContract. Contract key is:', searchContractKey);
+    alert('You\'ll now be routed to registration where you can register as a TradeOut user and accept this contract.');
+    dispatch({ type: 'SET_CONTRACT_KEY', payload: searchContractKey })
+    history.push('/registration');
+  }
+
   return (
     <div>
       <h1>RecipientView</h1>
       <ContractPreview contractDetails={contractDetails} />
       <div>
-          <br></br>
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Button 
-              variant="contained"
-              onClick={(event) => history.push('/registration')}
-              sx={{marginLeft: 1, width: 200}}
-              >
-              Accept
-            </Button>
-            <Button 
-              variant="contained"
-              // onClick={(event) => history.push('/registration')}
-              sx={{marginLeft: 1, width: 200}}
-              >
-              Decline
-            </Button>
-          </Box>
-        </div>
+        <br></br>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Button
+            variant="contained"
+            onClick={acceptContract}
+            sx={{ marginRight: 1, width: 200 }}
+          >
+            Accept
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ marginLeft: 1, width: 200 }}
+          >
+            Decline
+          </Button>
+        </Box>
+      </div>
     </div>
   );
 }
