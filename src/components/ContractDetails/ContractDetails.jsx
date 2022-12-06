4182450c8171f7b63660e7c5847c99bc6c7d8f02
Bryn Nadziejka Waller <bryn.m.nw@gmail.com>
2022-12-06 09:58:45 -0600

    edited code comments in ContractDetails

--- src/components/ContractDetails/ContractDetails.jsx
+++ src/components/ContractDetails/ContractDetails.jsx
@@ -54,7 +54,7 @@ function ContractDetails() {
           userAction ?  <div>
                           <Button
                             variant="contained"
-                            // onClick to updated contract_status and trigger PDF generation
+                            // onClick to update contract_status to 'accepted' and trigger PDF generation
                             sx={{ marginRight: 1, width: 200, height: 60 }}
                           >
                             Sign and Finalize Contract
@@ -62,7 +62,7 @@ function ContractDetails() {
                           <Button
                             variant="contained"
                             color="error"
-                            // onClick to update contract_status and return user to /dashboard
+                            // onClick to update contract_status to 'declined', trigger a declined contract confirmation alert, and return user to /dashboard
                             sx={{ marginLeft: 1, width: 200, height: 60 }}
                           >
                             Decline Contract
